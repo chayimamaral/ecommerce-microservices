8491a436bcfb0bad63c31227ddc78d1812fa5d03
Carlos Amaral <chayimamaral@gmail.com>
2025-09-30 17:07:16 -0300

    Refactoring Vendas

--- EstoqueService/Models/Produto.cs
+++ EstoqueService/Models/Produto.cs
@@ -4,7 +4,7 @@ public class Produto
 {
     public int Id { get; set; }
     public string Nome { get; set; } = string.Empty;
-    public string Descricao { get; set; }
-    public decimal Preco { get; set; }
-    public int Quantidade { get; set; }
+    public string Descricao { get; set; } = string.Empty;
+    public decimal Preco { get; set; } = 0;
+    public int Quantidade { get; set; } = 0;
 }
