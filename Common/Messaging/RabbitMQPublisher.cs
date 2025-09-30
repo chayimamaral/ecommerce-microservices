695a56538646d203d807cd7326b8785c6068eba0
Carlos Amaral <chayimamaral@gmail.com>
2025-09-30 11:20:00 -0300

    Refactoring do  RabgitMQPublisher.cs

--- Common/Messaging/RabbitMQPublisher.cs
+++ Common/Messaging/RabbitMQPublisher.cs
@@ -1,42 +1,41 @@
 using System.Text;
-using System.Text.Json;
 using RabbitMQ.Client;
 
 namespace Common.Messaging;
 
-public class RabbitMQPublisher
+public class RabbitMQPublisher : IDisposable
 {
     private readonly IConnection _connection;
-    private readonly IModel _channel;
 
-            public RabbitMQPublisher(string hostname = "localhost")
-        {
-            var factory = new ConnectionFactory() { HostName = hostname };
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
-        }
+    public RabbitMQPublisher(string hostname = "localhost")
+    {
+        var factory = new ConnectionFactory() { HostName = hostname };
+        _connection = factory.CreateConnection();
+    }
 
-        public void Publish(string queueName, string message)
-        {
-            _channel.QueueDeclare(
-                queue: queueName,
-                durable: false,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null);
+    public void Publish(string queueName, string message)
+    {
+        // Cria o canal temporariamente aqui, sem precisar da interface IModel
+        using var channel = _connection.CreateModel();
 
-            var body = Encoding.UTF8.GetBytes(message);
+        channel.QueueDeclare(
+            queue: queueName,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
 
-            _channel.BasicPublish(
-                exchange: "",
-                routingKey: queueName,
-                basicProperties: null,
-                body: body);
-        }
+        var body = Encoding.UTF8.GetBytes(message);
 
-        public void Dispose()
-        {
-            _channel?.Close();
-            _connection?.Close();
-        }
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: queueName,
+            basicProperties: null,
+            body: body);
+    }
+
+    public void Dispose()
+    {
+        _connection?.Close();
+    }
 }
